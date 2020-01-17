ab8999e381397a08fd1151fa5df9ea5b95283a59
Viktorija <viktorija.komske@gmail.com>
2020-01-17 14:11:39 +0200

    my-skills final

--- js/actions.js
+++ js/actions.js
@@ -1,22 +1,14 @@
 "use strict";
 
 // GALLERY
-
-<<<<<<< HEAD
-renderGallery( works );
-=======
 renderGallery('#latest_works_gallery', works);
->>>>>>> develop
 
 
 //achievements
 
 renderAchievements( achievements );
 
-<<<<<<< HEAD
 // my-skills
-
 renderSkills( skills );
-=======
+
 lightbox();
->>>>>>> develop
