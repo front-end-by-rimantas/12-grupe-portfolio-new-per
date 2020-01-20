5474b6769a7ebfa4eefc18fb17b8ef2dd7f40b9b
VilijaKveine <vilija.cizaite@gmail.com>
2020-01-19 18:01:22 -0800

    IMy-services initial commit

--- js/actions.js
+++ js/actions.js
@@ -1,21 +1,24 @@
 "use strict";
 
 //hero
+ manipulateLetter( spec, 0, 0, 'add');  
 
- manipulateLetter( spec, 0, 0, 'add');
-    
 // GALLERY
 renderGallery('#latest_works_gallery', works);
 
 
 //achievements
-
 renderAchievements( achievements );
 
 //Myresume
-
 renderMyresume( linesResume);
+
 // my-skills
 renderSkills( skills );
 
+// MY SERVICES
+
+renderServices( myServices );
+
+//lightbox
 lightbox();
